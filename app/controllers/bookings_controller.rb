5244de43055cedb3db9dbfdb572fd471187bb30e
lennard ypma <ypmalennard@hotmail.com>
2016-03-08 18:04:52 +0100

    adds booking controller, no redirection

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,41 @@
 class BookingController < ApplicationController
+  before_action :find_booking, only[:edit, :update, :destroy]
+
+  def index
+    @booking = Booking.all
+  end
+
   def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      #redirection
+    else
+      render :new
+    end
   end
 
   def edit
   end
+
+  def update
+    @booking.update(booking_params)
+    #redirection
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def booking_params
+  end
+
+  def find_booking
+  end
+
 end
