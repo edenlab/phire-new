0be9ec65db1411de81c545de36d2bee517270d91
WouterKompier <wouterjckompier@gmail.com>
2016-03-09 12:30:55 +0100

    add validation again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,15 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :location])
-    
+
     devise_parameter_sanitizer.for(:sign_up) << :first_name
     devise_parameter_sanitizer.for(:sign_up) << :last_name
     devise_parameter_sanitizer.for(:sign_up) << :location
+
+    # devise_parameter_sanitizer.for(:account_update) << :first_name
+    # devise_parameter_sanitizer.for(:account_update) << :last_name
+    # devise_parameter_sanitizer.for(:account_update) << :location
+
+
   end
 end
