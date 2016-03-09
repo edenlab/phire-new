aa13b43d79b913bd2f9334909f5125099b715ff6
lennard ypma <ypmalennard@hotmail.com>
2016-03-09 11:22:18 +0100

    adds and installs figaro, photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,22 +1,43 @@
 class PhotosController < ApplicationController
+  before_action :find_photo, only: [:edit, :update, :destroy]
+
   def index
+    @photos = Photo.all
   end
 
   def new
+    @photo = Photo.new
   end
 
   def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      #redirection
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @photo.update(photo_params)
+    #redirection
   end
 
   def destroy
+    @photo.destroy
+    #redirection
   end
 
   private
 
+  def photo_params
+    params.require(:photo).permit(:name, :specialty)
+  end
+
+  def find_photo
+    @photo = Photo.find(params[:id])
+  end
 end
